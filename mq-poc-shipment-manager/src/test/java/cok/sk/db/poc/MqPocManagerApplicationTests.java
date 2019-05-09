2f5ff2ee5c01a65a6b848751a44bc8e3515d2885
shishir-insane <shishir.insane@gmail.com>
2019-05-09 15:29:31 +0200

    Updated test case

--- mq-poc-shipment-manager/src/test/java/cok/sk/db/poc/MqPocManagerApplicationTests.java
+++ mq-poc-shipment-manager/src/test/java/cok/sk/db/poc/MqPocManagerApplicationTests.java
@@ -3,10 +3,14 @@ package cok.sk.db.poc;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sk.db.poc.shipment.MqPocManagerApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = MqPocManagerApplication.class)
 public class MqPocManagerApplicationTests {
 
 	@Test
